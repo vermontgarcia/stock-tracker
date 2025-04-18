e9e64512e6f76395a973df1bf98966f53a52e269
Vermont Garcia <vermontgarcia@gmail.com>
2025-04-18 09:39:27 -0600

    Header logo link

--- src/components/header.mjs
+++ src/components/header.mjs
@@ -4,12 +4,15 @@ export const setupHeader = (element) => {
     element.innerHTML = `
       <div>
         <div class="logo-container">
-          <img
-            src="/images/logo.svg"
-            alt="Stock Tracker App Logo"
-            width="64"
-            height="64"
-          />
+          <a href="/">
+            <img
+              class="logo"
+              src="/images/logo.svg"
+              alt="Stock Tracker App Logo"
+              width="64"
+              height="64"
+            />
+          </a>
           <div class="logo-name-container">
             <p>WDD330</p>
             <p>Stock Tracker App</p>
