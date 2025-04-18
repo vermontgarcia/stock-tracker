276ba70002327d2febcf88fae64b29da2d1a26cd
vermontgarcia <40477541+vermontgarcia@users.noreply.github.com>
2025-04-18 10:14:13 -0600

    Promote DEV to MASTER (#1)
    * Folder structure
    
    * Revert "Folder structure"
    
    This reverts commit bdb504a2116bfb2168b847224b3fcd1290da02ae.
    
    * Folder structure
    
    * Layout
    
    * Header logo link
    
    * Layout styles
    
    * Delete
    
    * Ignore update
    
    ---------
    
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>

---
+++ src/components/header.mjs
@@ -0,0 +1,32 @@
+export const setupHeader = (element) => {
+  const page = 'home';
+  const setHeader = () => {
+    element.innerHTML = `
+      <div>
+        <div class="logo-container">
+          <a href="/">
+            <img
+              class="logo"
+              src="/images/logo.svg"
+              alt="Stock Tracker App Logo"
+              width="64"
+              height="64"
+            />
+          </a>
+          <div class="logo-name-container">
+            <p>WDD330</p>
+            <p>Stock Tracker App</p>
+          </div>
+        </div>
+        <div class="menu-btn">
+          <a id="menu-btn" role="button" class="menu-icon">
+            <i id="menu-icon" class="material-icons">menu</i>
+          </a>
+        </div>
+      </div>
+      <nav class="menu" id="menu">
+      </nav>
+    `;
+  };
+  setHeader();
+};
---
+++ src/main.mjs
@@ -0,0 +1,16 @@
+import './styles/main.css';
+import './styles/large.css';
+import { setupHeader } from './components/header.mjs';
+import { setupFooter } from './components/footer.mjs';
+import { setupNavigation } from './components/navigation.mjs';
+
+document.querySelector('#app').innerHTML = `
+  <header></header>
+  <main>
+  </main>
+  <footer></footer>
+`;
+
+setupHeader(document.querySelector('header'));
+setupFooter(document.querySelector('footer'));
+setupNavigation(document.querySelector('nav'));
