9521fe3642265ed0a462fecc72b786ca866a0b5e
Vermont Garcia <vermontgarcia@gmail.com>
2025-04-19 10:01:00 -0600

    API Consts

--- src/utils/consts.env.mjs
+++ src/utils/consts.env.mjs
@@ -1,3 +1,7 @@
 export const FINNHUB_API_KEY = import.meta.env.VITE_FINNHUB_API_KEY;
 export const FINNHUB_BASE_URL = import.meta.env.VITE_FINNHUB_BASE_URL;
 export const FINNHUB_WS_URL = import.meta.env.VITE_FINNHUB_WS_URL;
+export const ALPHA_BASE_URL = import.meta.env.VITE_ALPHA_BASE_URL;
+export const ALPHA_API_KEY = import.meta.env.VITE_ALPHA_API_KEY;
+export const ALPACA_BASE_URL = import.meta.env.VITE_ALPACA_BASE_URL;
+export const ALPACA_API_KEY = import.meta.env.VITE_ALPACA_API_KEY;
