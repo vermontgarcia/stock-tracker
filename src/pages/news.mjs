7d0aaf0c77fb7e9d4ea23778fdba627eb42e6915
vermontgarcia <40477541+vermontgarcia@users.noreply.github.com>
2025-04-18 22:22:53 -0600

    News fix (#6)
    * Folder structure
    
    * Revert "Folder structure"
    
    This reverts commit bdb504a2116bfb2168b847224b3fcd1290da02ae.
    
    * Folder structure
    
    * Layout
    
    * Header logo link
    
    * Layout styles
    
    * Delete
    
    * Ignore update
    
    * Social Logos
    
    * Navigation and Footer made dynamic and responsive
    
    * Routing pages
    
    * Fallback to index
    
    * Layout
    og data
    assets
    consts
    components
    news
    
    * News page
    
    * News temp data removed
    
    ---------
    
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>

--- src/pages/news.mjs
+++ src/pages/news.mjs
@@ -12,28 +12,8 @@ import { capitalize, setPageMetadata } from '../utils/utils.mjs';
 import { setupHeroImg } from '../components/heroImg.mjs';
 import { beforeEnd, news } from '../utils/consts.mjs';
 import { newsCardTemplate } from '../utils/templates.mjs';
-import { newsData } from '../utils/tempData.mjs';
 
 const finnHubAPIClient = new FinnHubAPIClient();
-
-export const renderNews = () => {
-  document.querySelector('#app').innerHTML = `
-    <header></header>
-    <main><div id="news-page"></div></main>
-    <footer></footer>
-  `;
-  setPageMetadata(news);
-  setupHeader(document.querySelector('header'));
-  setupFooter(document.querySelector('footer'));
-  setupNavigation(document.querySelector('nav'), news);
-  setupHeroImg(document.querySelector('#news-page'), {
-    small: heroNewsSmall,
-    medium: heroNewsMedium,
-    large: heroNewsLarge,
-    title: capitalize(news),
-  });
-  getNews('crypto');
-};
 
 const getNews = async (category = 'crypto') => {
   const news = await finnHubAPIClient.searchNewsByCategory(category);
@@ -42,8 +22,24 @@ const getNews = async (category = 'crypto') => {
     .filter((item) => item.summary !== '')
     .map(newsCardTemplate)
     .join('')}</div>`;
+  document.querySelector('main')?.insertAdjacentHTML(beforeEnd, newsContainer);
+};
 
-  document
-    .querySelector('#news-page')
-    ?.insertAdjacentHTML(beforeEnd, newsContainer);
+export const renderNews = () => {
+  document.querySelector('#app').innerHTML = `
+    <header></header>
+    <main></main>
+    <footer></footer>
+  `;
+  setPageMetadata(news);
+  setupHeader(document.querySelector('header'));
+  setupFooter(document.querySelector('footer'));
+  setupNavigation(document.querySelector('nav'), news);
+  setupHeroImg(document.querySelector('main'), {
+    small: heroNewsSmall,
+    medium: heroNewsMedium,
+    large: heroNewsLarge,
+    title: capitalize(news),
+  });
+  getNews('crypto');
 };
